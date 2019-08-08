36767351198c4e230cb794080c9219134670a1e0
dlilien <skinonframe@gmail.com>
2019-08-08 16:47:40 +0200

    Reduce file size

--- img/pole_1819/fig2jpg.sh
+++ img/pole_1819/fig2jpg.sh
@@ -7,4 +7,4 @@
 #out=${in%.*}_sm.jpg
 #convert $in -units PixelsPerInch -antialias -resize 1500x1500 -density 150 -quality 85 $out
 
-parallel "convert {} -units PixelsPerInch -antialias -resize 1500x1500 -density 330 -quality 85 {.}_sm.jpg" ::: $@
+parallel "convert {} -units PixelsPerInch -antialias -resize 1500x1500 -density 330 -quality 85 {.}.jpg" ::: $@
