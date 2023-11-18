c16203c01416d7b164c6a4efcc2cd58bee66241b
David Lilien <david.lilien@umanitoba.ca>
2023-11-18 10:38:41 -0500

    A couple new publications

--- img/wa_glaciers/thumbify.sh
+++
@@ -1,8 +0,0 @@
-rm -f *-thumb.jpg
-if [ ! -d thumbs ]; then
-    mkdir thumbs
-fi
- 
-mogrify  -format jpg -path thumbs -thumbnail 250x250 *.jpg
-
-for f in `ls thumbs/*jpg`; do e=`basename $f`; mv $f ${e%.*}-thumb.jpg; done
